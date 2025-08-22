c287bc38b263b0cb1227d23516a20b934adfa855
adrianefrndz <adrianefrndz07@gmail.com>
2025-08-23 05:25:08 +0800

    Updating

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Arwas-Church-Of-Christ/',
   plugins: [react()],
+  base: '/Arwas-Church-Of-Christ/',
 })
